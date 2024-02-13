a99446f90ac4f21040ec800708a76552bcee04ad
ALT021 <penbead@outlook.com>
2024-02-13 14:45:34 +0800

    Site updated: 2024-02-13 14:45:32

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["/0001.html","/0002.html","/0003.html","/0004.html","/0005.html","/0006.html","/0007.html","/0008.html","/0009.html","/0010.html","/0011.html","/0012.html"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
